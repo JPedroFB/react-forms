1d39bbe3f50a26a89d50343182e7a4019baa4610
JPedroFB <49993093+JPedroFB@users.noreply.github.com>
2023-05-24 01:15:45 -0300

    1 file modified

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -1,17 +1,33 @@
+import { Card, CardContent, CardHeader, Grid } from '@mui/material';
 import * as React from 'react';
 import './Product.scss';
 
 const Product = ({ data }) => {
   return (
-    <div className="productCard">
-      <div className="productTitle">
-        <h2>{data.name}</h2>
-      </div>
-      <div className="productImages">
-        <img src={data.image} />
-      </div>
-      <div className="productPrice">
-        <span>{data.price}</span>
+    <div>
+      <Card variant="outlined">
+        <CardHeader title={data.name} />
+        <CardContent>
+          <Grid container>
+            <Grid cotainer justifyContent="flex-end" item xs={12}>
+              <img src={data.image} />
+            </Grid>
+            <Grid container justifyContent="flex-end">
+              <span>{data.price}</span>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
+      <div className="productCard">
+        <div className="productTitle">
+          <h2>{data.name}</h2>
+        </div>
+        <div className="productImages">
+          <img src={data.image} />
+        </div>
+        <div className="productPrice">
+          <span>{data.price}</span>
+        </div>
       </div>
     </div>
   );
